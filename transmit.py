f01cb7238ec1c129d2e6e456a2fffd5040ca5781
Dali Basor <97993306+basor-dali@users.noreply.github.com>
2024-02-04 18:45:48 -0600

    Added message count to each msg sent

--- transmit.py
+++ transmit.py
@@ -3,11 +3,14 @@ import serial
 
 lora = serial.Serial(port='/dev/ttyS0', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 
-message = "Hello LoRa node"  # Default msg to be published
+message = "Hello LoRa node"  # Define your message here
 b = bytes(message, 'utf-8')
+message_count = 0  # Initialize the message counter
 
 while True:
     s = lora.write(b)  # Send the data to other lora
-    print("Message sent:", message)
+    message_count += 1  # Increment the message count
+    print(f"Message {message_count} sent: {message}")  # Include the message count in the print statement
     time.sleep(0.2)  # Delay of 200ms
+
 
