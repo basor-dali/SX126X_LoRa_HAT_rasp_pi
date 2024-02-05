76b61a0d03f5f7d77f79a8554f83634423d064a6
Dali Basor <97993306+basor-dali@users.noreply.github.com>
2024-02-04 18:22:18 -0600

    Added default message
    Added default msg to be sent every 200ms without waiting for user input

--- transmit.py
+++ transmit.py
@@ -3,8 +3,11 @@ import serial
 
 lora = serial.Serial(port='/dev/ttyS0', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 
+message = "Hello LoRa node"  # Default msg to be published
+b = bytes(message, 'utf-8')
+
 while True:
-    n = input("Enter The Message = ")  # input the string
-    b = bytes(n, 'utf-8')  # convert string into bytes
-    s = lora.write(b)  # send the data to other lora
-    time.sleep(0.2)  # delay of 200ms
+    s = lora.write(b)  # Send the data to other lora
+    print("Message sent:", message)
+    time.sleep(0.2)  # Delay of 200ms
+
